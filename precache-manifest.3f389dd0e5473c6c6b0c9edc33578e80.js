36a749f24ddf356bdedcbe814d0e1cdea2d3e282
Wai30Yan <42363348+Wai30Yan@users.noreply.github.com>
2022-02-08 14:18:42 +1100

    Updates

---
+++ precache-manifest.3f389dd0e5473c6c6b0c9edc33578e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93df832bfae4bf35ca391142f81a5d9b",
+    "url": "/react_joke_app/index.html"
+  },
+  {
+    "revision": "fab1b4869731a9e0a9b0",
+    "url": "/react_joke_app/static/css/main.fc720f1d.chunk.css"
+  },
+  {
+    "revision": "5c6e240379c785ab476b",
+    "url": "/react_joke_app/static/js/2.4ae7a89d.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react_joke_app/static/js/2.4ae7a89d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab1b4869731a9e0a9b0",
+    "url": "/react_joke_app/static/js/main.83ec9843.chunk.js"
+  },
+  {
+    "revision": "4a4472c809fd1a0fb735",
+    "url": "/react_joke_app/static/js/runtime-main.77467d10.js"
+  }
+]);
